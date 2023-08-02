4f87b16f75fbffaf093b85bc61c8726bae2bf5aa
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-08-02 17:24:30 +0530

    don't send pull about config

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ MONGO_DB_URL = getenv("MONGO_DB_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", 5938660179))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
 
-LOGGER_ID = getenv("LOGGER_ID", None)
+LOGGER_ID = getenv("LOGGER_ID", "-1001819078701")
