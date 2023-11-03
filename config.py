9700fe1f6570b3b12e53c72feaa805bd3c1b4492
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2023-11-02 21:03:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("20515794"))
+API_HASH = getenv("da128bd223a333f5bde8dc1359db4609")
 
-TOKEN = getenv("TOKEN")
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+TOKEN = getenv("6510303912:AAE2k8DFC7Ot_ifKayMeXoTtcrWerTwPs8w")
+MONGO_DB_URL = getenv("mongodb+srv://synaxxkhushi:synaxherebaby@cluster0.vqzfrg0.mongodb.net/?retryWrites=true&w=majority", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 6231550362))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/synaxchatgroup")
 
-LOGGER_ID = getenv("LOGGER_ID", "-1001819078701")
+LOGGER_ID = getenv("LOGGER_ID", "-1002132398644")
 DEV_USERS = set(int(x) for x in getenv("DEV_USERS", "").split())
